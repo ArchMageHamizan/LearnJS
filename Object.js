75cb32a47c722d35a697dd3e61904d41ccee55a4
Hamizan <hamizan@hla-group.com>
2025-03-17 11:43:58 +0800

    Grouping things together into objects

--- Object.js
+++ Object.js
@@ -1,10 +1,15 @@
 //create an object
+console.log("---create an object---");
 const myObject={
     name:"Hamizan",
     age:25,
     "Interest":function(){
         console.log("I am interested in coding");
     }
+}
+const myObject2={
+    name:"Irfan",
+    age:25,
 }
 const varFetch="Interest";
 //ways to access object properties
@@ -15,4 +20,23 @@ console.log("4/",myObject["Interest"]);
 console.log("5/",myObject.varFetch); //gives us 'undefined' because it's looking for a property named 'variable' in our object
 console.log("6/",myObject["varFetch"]); //gives us 'undefined' because it's looking for a property named 'variable' in our object
 console.log("7/",myObject[varFetch]);
+console.log("8/",myObject2.name);
 
+
+//Grouping things together into objects
+console.log("---Grouping things together into objects---");
+const playerOne={
+    name:"Hamizan",
+    marker:"X"
+};
+const playerTwo={
+    name:"Alya",
+    marker:"O"
+};
+function printPlayerName(num,player){
+    console.log(num,"/ ",player.name," marker is ",player.marker);
+};
+printPlayerName(1,playerOne);
+printPlayerName(2,playerTwo);
+
+      
